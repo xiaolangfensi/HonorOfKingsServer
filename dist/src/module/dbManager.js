bc8848da0a9684645dee90afed2da04cc227ae18
xiaolangfensi <xiaolangfensi@foxmail.com>
2019-11-30 22:50:18 +0800

    课时59；

--- dist/src/module/dbManager.js
+++ dist/src/module/dbManager.js
@@ -3,14 +3,15 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const mysqlpool_1 = require("../common/mysqlpool");
 const mylogger_1 = require("../common/mylogger");
 class DBManager {
+    constructor() {
+        this._sqlQueryAcountUser = 'select id,cdkey cdKey,user_name userName,cs_id csId,sdk_is sdkId,password from fball_accountdb.account_user where cdkey = ?';
+        this._mysql = {};
+    }
     static getInstance() {
         if (!this._instance) {
             this._instance = new DBManager();
         }
         return this._instance;
-    }
-    constructor() {
-        this._mysql = {};
     }
     initialize(confFile) {
         this._mysql = new mysqlpool_1.MysqlPool(confFile);
@@ -18,6 +19,9 @@ class DBManager {
     }
     executeSQL(sqlStr, sqlParam = []) {
         return this._mysql.query(sqlStr, sqlParam);
+    }
+    readUser(cdKey) {
+        return this._mysql.query(this._sqlQueryAcountUser, [cdKey]);
     }
 }
 exports.DBManager = DBManager;
--- src/module/dbmanager.ts
+++ src/module/dbmanager.ts
@@ -9,18 +9,23 @@ export class DBManager {
             this._instance = new DBManager();
         }
         return this._instance;
-    }    
+    }
 
-    private constructor () {
+    private constructor() {
         this._mysql = <MysqlPool>{};
     }
 
-    public initialize(confFile:DBConfig) {
+    public initialize(confFile: DBConfig) {
         this._mysql = new MysqlPool(confFile);
         myLogger.log('DBManager initialize finished ...');
     }
 
-    executeSQL(sqlStr:string, sqlParam = []): Promise<any> {
+    executeSQL(sqlStr: string, sqlParam = []): Promise<any> {
         return this._mysql.query(sqlStr, sqlParam);
+    }
+
+    private _sqlQueryAcountUser = 'select id,cdkey cdKey,user_name userName,cs_id csId,sdk_is sdkId,password from fball_accountdb.account_user where cdkey = ?';
+    private readUser(cdKey: string): Promise<any> {
+        return this._mysql.query(this._sqlQueryAcountUser, [cdKey]);
     }
 }
